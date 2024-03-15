ce686841df4bc11aabd97251b4e34dd537f819fb
dambatenne <dambatenne@gmail.com>
2024-03-15 11:44:34 +0100

    Added <Image/> to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+import homeImg from "public/home.jpg"
 
 export default function Home() {
   return (
     <div>
       Home page
+      <Image
+        src={homeImg}
+        alt="car factory"
+        // fill
+        // style={{ objectFit: 'cover' }}
+      />
     </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div>
-      <Link href="/">Home</Link>
+      <Link href="/">home</Link>
       <Link href="performance">Performance</Link>
       <Link href="reliability">Reliability</Link>
       <Link href="scale">Scale</Link>
